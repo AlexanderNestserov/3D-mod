091b4c52336321b89b491ceaa0edb343cb03b789
Alexander Nestserov <alexerdeader@gmail.com>
2021-11-21 13:05:42 +0300

    leso25

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,36 +4,22 @@ const modal = () => {
    const popupBtn = document.querySelectorAll('.popup-btn');
    const modal = document.querySelector('.popup');
    const popupContent = document.querySelector('.popup-content');
-   let count = 0;
-   let idInterval;
-
-   /* const animate = () => {
-       count++;
-       idInterval = requestAnimationFrame(animate);
-       if (count < 100 && screen.availWidth >= 768) {
-          popupContent.style.top = count + 'px';
-       } else {
-          cancelAnimationFrame(idInterval);
-       }
-    };*/
 
    popupBtn.forEach(btn => btn.addEventListener('click', () => {
-      //idInterval = requestAnimationFrame(animate);
-      animate({
-
-         duration: 1000,
-         timing(x, timeFraction) {
-            return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x);
-         },
-         draw(progress) {
-            modal.style.display = "block";
-            popupContent.style.left = (40 * progress) + '%';
-            popupContent.style.top = (25 * progress) + '%';
-            popupContent.style.opacity = progress;
-         }
-      });
-      //modal.style.display = "block";
-      //count = 0;
+      if (window.innerWidth >= 768) {
+         animate({
+            duration: 1000,
+            timing(x, timeFraction) {
+               return Math.pow(timeFraction, 2) * ((x + 1) * timeFraction - x);
+            },
+            draw(progress) {
+               modal.style.display = "block";
+               popupContent.style.left = (40 * progress) + '%';
+               popupContent.style.top = (25 * progress) + '%';
+               popupContent.style.opacity = progress;
+            }
+         });
+      }
    }));
 
    modal.addEventListener('click', (e) => {
@@ -41,5 +27,6 @@ const modal = () => {
          modal.style.display = "none";
       }
    });
+
 };
 export default modal;
